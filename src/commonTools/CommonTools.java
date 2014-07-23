c2031e4c2cc7a5146a2f4af873b46f0fb6673a9f
ToxicBard <limpbizkitfan943@yahoo.com>
2014-07-23 01:48:56 -0400

    -Finished consolidating the functionality of loading/asking/saving a directory into one function that can be called anywhere -Put I/O related things into a new class titled FileTools

--- src/commonTools/CommonTools.java
+++ src/commonTools/CommonTools.java
@@ -10,11 +10,6 @@ import java.io.IOException;
 
 import javax.swing.JFileChooser;
 
-/*
- * TODO Abstract/Generalize the load/select/save
- * directory functionality from DirectoryCompare and
- * add it here
- */
 
 public class CommonTools {
 
@@ -23,127 +18,9 @@ public class CommonTools {
 		System.exit(0);
 	}
 	
-	public static BufferedReader loadReadFile(String filePath){
-		File myFile = new File(filePath);
-		FileReader myFileReader = null;
-		BufferedReader br = null;
-		
-		//Only try to load if the save file actually exists
-		if(myFile.exists()){
-			//Try to open the file.  If the file exists but can't
-			//be opened, then tell the user and exit.
-			try {
-				myFileReader = new FileReader(myFile);
-			} catch (FileNotFoundException e) {
-				CommonTools.processError("Error loading file from disk.");
-			}
-			
-			//Create a BufferedReader from the already-opened file
-			br = new BufferedReader(myFileReader);
-			
-			return br;
-		}
-		
-		//Return a null value if the file doesn't exist.
-		return null;
-	}
-	
-	public static BufferedWriter openWriteFile(String filePath){
-		File myFile = new File(filePath);
-		return openWriteFile(myFile);
-	}
-	
-	public static BufferedWriter openWriteFile(File writeFile){
-		FileWriter myFileWriter = null;
-		BufferedWriter bw;
-
-		try {
-			//If the parent directory doesn't exist, then create it
-			if(!writeFile.getParentFile().exists()){
-				writeFile.getParentFile().mkdirs();
-			}
-			
-			//If the file doesn't exist, then create it.
-			if(!writeFile.exists()){
-				writeFile.createNewFile();
-			}
-		} catch (IOException e) {
-			CommonTools.processError("Error creating save file.");
-		}
-		
-		//Open the file for writing whether it already existed or not.
-		try {
-			myFileWriter = new FileWriter(writeFile);
-		} catch (IOException e1) {
-			CommonTools.processError("Error Opening File Writer.");
-		}
-		
-		bw = new BufferedWriter(myFileWriter);
-		
-		return bw;
-	}
-	
 	public static boolean isDebugMode(){
 		return java.lang.management.ManagementFactory.getRuntimeMXBean().
 			    getInputArguments().toString().indexOf("jdwp") >= 0;
 	}
 	
-	public static File selectSavedDirectory(String saveFilePath){
-		File loadedFile = loadSavedDirectory(saveFilePath);
-	}
-	
-	public static File loadSavedDirectory(String saveFilePath){
-		BufferedReader br = CommonTools.loadReadFile(saveFilePath);
-		File myDirectory = null;
-		String inputDirectory = null;
-		
-		//Only try to read the file if it actually exists
-		if(br != null){
-
-			try {
-				inputDirectory = br.readLine();
-			} catch (IOException e) {
-				CommonTools.processError("Error reading File.");
-			}
-			
-			myDirectory = new File(inputDirectory);
-			
-			/*
-			 * If the filename provided by the file doesn't exist or isn't a directory
-			 * then clear out the directory variable.
-			 * In this case we want to fail silently and continue execution.
-			 */
-			if(myDirectory.exists() == false || myDirectory.isDirectory() == false){
-				myDirectory = null;
-			}
-			
-			try {
-				br.close();
-			} catch (IOException e) {
-				CommonTools.processError("Error closing file reader");
-			}
-		}
-		
-		return myDirectory;
-	}
-	
-	private static File getDirectoryFromUser(String dialogTitle, File loadedFile){
-		
-	}
-	
-	//Asks the user for a directory based on a starting point, which has presumably been loaded from disk.
-	private static File askUserForDirectory(String dialogTitle, File startingDirectory){
-		JFileChooser fileChooser = new JFileChooser();
-		
-		fileChooser.setCurrentDirectory(startingDirectory);
-		fileChooser.setDialogTitle(dialogTitle);
-		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		fileChooser.setAcceptAllFileFilterUsed(false);
-		
-		if(fileChooser.showOpenDialog(fileChooser) == JFileChooser.APPROVE_OPTION){
-			return fileChooser.getSelectedFile();
-		}
-		
-		return null;
-	}
 }
---
+++ src/commonTools/FileTools.java
@@ -0,0 +1,193 @@
+package commonTools;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+
+public class FileTools {
+
+	public static BufferedReader loadReadFile(String filePath){
+		File myFile = new File(filePath);
+		FileReader myFileReader = null;
+		BufferedReader br = null;
+		
+		//Only try to load if the save file actually exists
+		if(myFile.exists()){
+			//Try to open the file.  If the file exists but can't
+			//be opened, then tell the user and exit.
+			try {
+				myFileReader = new FileReader(myFile);
+			} catch (FileNotFoundException e) {
+				CommonTools.processError("Error loading file from disk.");
+			}
+			
+			//Create a BufferedReader from the already-opened file
+			br = new BufferedReader(myFileReader);
+			
+			return br;
+		}
+		
+		//Return a null value if the file doesn't exist.
+		return null;
+	}
+	
+	public static BufferedWriter openWriteFile(String filePath){
+		File myFile = new File(filePath);
+		return openWriteFile(myFile);
+	}
+	
+	public static BufferedWriter openWriteFile(File writeFile){
+		FileWriter myFileWriter = null;
+		BufferedWriter bw;
+
+		try {
+			//If the parent directory doesn't exist, then create it
+			if(!writeFile.getParentFile().exists()){
+				writeFile.getParentFile().mkdirs();
+			}
+			
+			//If the file doesn't exist, then create it.
+			if(!writeFile.exists()){
+				writeFile.createNewFile();
+			}
+		} catch (IOException e) {
+			CommonTools.processError("Error creating save file.");
+		}
+		
+		//Open the file for writing whether it already existed or not.
+		try {
+			myFileWriter = new FileWriter(writeFile);
+		} catch (IOException e1) {
+			CommonTools.processError("Error Opening File Writer.");
+		}
+		
+		bw = new BufferedWriter(myFileWriter);
+		
+		return bw;
+	}
+
+	public static File selectSavedDirectory(String dialogTitle, String saveFilePath){
+		File loadedFolder = null;
+		File selectedFolder = null;
+		String loadedFolderPath = "";
+		String selectedFolderPath = "";
+		
+		//Load directory from disk if it exists
+		loadedFolder = loadSavedDirectory(saveFilePath);
+		
+		/*
+		 * Allow the user to select a directory with a filechooser
+		 * that starts out at the loaded location
+		 */
+		selectedFolder = getDirectoryFromUser(dialogTitle, loadedFolder);
+		
+		/*
+		 * Store a value for the paths of the loaded file and the
+		 * selected file so that they can be compared without worrying
+		 * about null values
+		 */
+		if(loadedFolder != null){
+			loadedFolderPath = loadedFolder.getAbsolutePath();
+		}
+		if(selectedFolder != null){
+			selectedFolderPath = selectedFolder.getAbsolutePath();
+		}
+		
+		/*
+		 * If the user's selection is different from what was 
+		 * loaded from disk, then save the new value to disk.
+		 */
+		if(!loadedFolderPath.equals(selectedFolderPath)){
+			saveDirectoryToDisk(saveFilePath, selectedFolder);
+		}
+		
+		return selectedFolder;
+		
+	}
+	
+	private static File loadSavedDirectory(String saveFilePath){
+		BufferedReader br = loadReadFile(saveFilePath);
+		File myDirectory = null;
+		String inputDirectory = null;
+		
+		//Only try to read the file if it actually exists
+		if(br != null){
+
+			try {
+				inputDirectory = br.readLine();
+			} catch (IOException e) {
+				CommonTools.processError("Error reading File.");
+			}
+			
+			myDirectory = new File(inputDirectory);
+			
+			/*
+			 * If the filename provided by the file doesn't exist or isn't a directory
+			 * then clear out the directory variable.
+			 * In this case we want to fail silently and continue execution.
+			 */
+			if(myDirectory.exists() == false || myDirectory.isDirectory() == false){
+				myDirectory = null;
+			}
+			
+			try {
+				br.close();
+			} catch (IOException e) {
+				CommonTools.processError("Error closing file reader");
+			}
+		}
+		
+		return myDirectory;
+	}
+	
+	private static File getDirectoryFromUser(String dialogTitle, File loadedFile){
+		File selectedFile = null;
+		
+		//Allow the user to select the directory.
+		//Use the existing directory Files as the starting directory because
+		//by this point they've been loaded from the save file if relevant.
+		selectedFile = askUserForDirectory(dialogTitle, loadedFile);
+		if(selectedFile == null){
+			//If the user didn't pick anything, then we exit the program because we have nothing to go on.
+			CommonTools.processError("You must select the former directory.");
+		}
+		
+		return selectedFile;
+	}
+	
+	//Asks the user for a directory based on a starting point, which has presumably been loaded from disk.
+	private static File askUserForDirectory(String dialogTitle, File startingDirectory){
+		JFileChooser fileChooser = new JFileChooser();
+		
+		fileChooser.setCurrentDirectory(startingDirectory);
+		fileChooser.setDialogTitle(dialogTitle);
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		fileChooser.setAcceptAllFileFilterUsed(false);
+		
+		if(fileChooser.showOpenDialog(fileChooser) == JFileChooser.APPROVE_OPTION){
+			return fileChooser.getSelectedFile();
+		}
+		
+		return null;
+	}
+	
+	private static void saveDirectoryToDisk(String saveFilePath, File selectedFolder){
+		BufferedWriter bw = openWriteFile(saveFilePath);
+		
+		//Write to and close the file.
+		try {
+			bw.write(selectedFolder.getAbsolutePath());
+			bw.newLine();
+			bw.close();
+		} catch (IOException e) {
+			CommonTools.processError("Error writing directory");
+		}
+		
+	}
+}
